44716704def592332ceccdface0d3d8412b8eacd
Sezer Sanchez <sezersanchez@sezer-macbook-pro.local>
2018-10-14 11:55:16 +0300

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Sezer Sanchez on 14.10.2018.
+//  Copyright © 2018 Cetin Sezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
